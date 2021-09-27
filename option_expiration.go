92378e0721580bc3c99608560231fe88b389fbca
storezhang <storezhang@gmail.com>
2021-09-27 11:37:04 +0800

    修复各种选项

--- option_expiration.go
+++ option_expiration.go
@@ -4,7 +4,15 @@ import (
 	`time`
 )
 
-var _ option = (*optionExpiration)(nil)
+var (
+	_ option         = (*optionExpiration)(nil)
+	_ valuesOption   = (*optionExpiration)(nil)
+	_ fieldOption    = (*optionExpiration)(nil)
+	_ countOption    = (*optionExpiration)(nil)
+	_ rangeOption    = (*optionExpiration)(nil)
+	_ membersOption  = (*optionExpiration)(nil)
+	_ intervalOption = (*optionExpiration)(nil)
+)
 
 type optionExpiration struct {
 	expiration time.Duration
@@ -25,5 +33,29 @@ func KeepExpiration() *optionExpiration {
 }
 
 func (e *optionExpiration) apply(options *options) {
+	options.expiration = e.expiration
+}
+
+func (e *optionExpiration) applyField(options *fieldOptions) {
+	options.expiration = e.expiration
+}
+
+func (e *optionExpiration) applyValues(options *valuesOptions) {
+	options.expiration = e.expiration
+}
+
+func (e *optionExpiration) applyRange(options *rangeOptions) {
+	options.expiration = e.expiration
+}
+
+func (e *optionExpiration) applyCount(options *countOptions) {
+	options.expiration = e.expiration
+}
+
+func (e *optionExpiration) applyMembers(options *membersOptions) {
+	options.expiration = e.expiration
+}
+
+func (e *optionExpiration) applyInterval(options *intervalOptions) {
 	options.expiration = e.expiration
 }
